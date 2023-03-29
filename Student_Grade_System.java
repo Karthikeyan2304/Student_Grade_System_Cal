3977f3e5e6efa25288b63000c9e6a8f9e7d0f5ce
KarthiKeyan Y <117830061+Karthikeyan2304@users.noreply.github.com>
2023-03-29 19:09:55 +0530

    Update Student_Grade_System.java

--- Student_Grade_System.java
+++ Student_Grade_System.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Student_Grade_System {
 
 	public static void main(String[] args) {
-		  
+		
 System.out.println("****************************Welcome To Grade System App**************************\n");
 			    Scanner sc = new Scanner (System.in);
 			    System.out.println("Enter The Maths Mark Of The Student(100)"); 
@@ -17,39 +17,51 @@ System.out.println("****************************Welcome To Grade System App*****
                 System.out.println("Enter The Computer Mark Of The Student(100)"); 
                 int comp=sc.nextInt() ;
                 System.out.println("Enter The Tamil Mark Of The Student(100)"); 
-                int tamil=sc.nextInt() ;
-                System.out.println("\n Grade Of The Student:"+gradeSystem(TotalMark(maths, science, social, comp, tamil)));
+                int tamil=sc.nextInt() ; 
+              
+                //  Passing  TotalMark() Result in Tmark(Varaible)
+                
+                int Tmark=TotalMark(maths, science, social, comp, tamil);
+                System.out.println("\nTotal Mark:"+Tmark);
+               
+                
+                // Passing gradeSystem() Result in Sperformer(Varaible);
+                
+                
+                String Sperformer=gradeSystem(Tmark);
+                System.out.println("\nGrade Of The Student:"+Sperformer);
+                
 	}
+	
 	public static int TotalMark(int maths,int science,int social,int comp,int tamil) 
 	{
-		return maths+science+social+comp+tamil;
-		
+	 
+	return  maths+science+social+comp+tamil;
 	} 
 	
-	public static String gradeSystem(int TotalMark) 
+	public static String gradeSystem(int TMark) 
 	{
 		String Performance="";
 		
-		if(TotalMark >= 400 && TotalMark <= 500)
+		if(TMark >= 400 && TMark <= 500)
 		{
 					Performance="High Performer";
 		}
-		else if(TotalMark >= 300 && TotalMark <= 399)
+		else if(TMark >= 300 && TMark <= 399)
 		{
 			Performance="Good Performer";
 		}
-		else if(TotalMark >= 200 && TotalMark <= 299)
+		else if(TMark >= 200 && TMark <= 299)
 		{
 			Performance="Average Performer";
 		}
-		else if(TotalMark >= 0 && TotalMark <= 199)
+		else if(TMark >= 0 && TMark <= 199)
 		{
 			Performance="Poor Performer";
 		}
 	return Performance;
 	}
+
 }
-
-
 
 
